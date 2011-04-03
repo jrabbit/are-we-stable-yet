8834b67a150a65dfea89c666d0a7f3d63bec4c41
Jrabbit <jackjrabbit+github@gmail.com>
2011-04-03 01:49:28 -0500

    slap a logo on it

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function loadnightlies(data){
     console.log(data);
-    $('footer').text("This was last updated at: " +  data['last-edit']);
+    $('#edited').text("This was last updated at: " +  data['last-edit'] );
     // var info = JSON.parse(data.responseText)
     // for (var i = 0; i < info.length; i++) {
     //     makeBox(info[i], )
